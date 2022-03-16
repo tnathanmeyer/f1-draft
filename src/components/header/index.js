d97af6489bdceb57ea318332ff84df6cc7ac1394
Nathan Meyer <55331718+tnathanmeyer@users.noreply.github.com>
2022-03-16 09:50:32 -0400

    Update header classes and styles i guess

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,14 @@
 import { h } from 'preact';
-import style from './style';
+import './style';
 
 const Header = () => {
 	return (
-		<div className={style.header}>
+		<header>
 			<img 
 				src='https://www.formula1.com/etc/designs/fom-website/images/f1_logo.svg'
 				alt='Formula 1 Logo'
 			/>
-		</div>
+		</header>
 	)
 }
 
