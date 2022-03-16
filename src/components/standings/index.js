92e2a1eef0c7093366517afa672c18f14de6ba08
Nathan Meyer <55331718+tnathanmeyer@users.noreply.github.com>
2022-03-16 10:50:49 -0400

    Refactor logic and stuff for columns

--- src/components/standings/index.js
+++ src/components/standings/index.js
@@ -1,72 +1,23 @@
 import { h } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
+import useStandings from '../../hooks/useStandings';
 import Column from '../column';
+import style from './style';
 
 const Standings = () => {
-	const [loading, setLoading] = useState(true);
-	const [players, setPlayers] = useState(null)
-	const [drivers, setDrivers] = useState(null);
-	const [points, setPoints] = useState(null);
-
-	useEffect(() => {
-		const getData = async () => {
-			const draftsPromise = import('../../data/drafts');
-			const apiPromise = fetch('https://ergast.com/api/f1/2022/driverStandings.json');
-
-			const [draftsModule, response] = await Promise.all([draftsPromise, apiPromise]);
-			const data = await response.json();
-			const drafts = draftsModule.default;
-			const players = Object.keys(drafts);
-
-			const driverData = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-			const formattedData = driverData.map(driver => {
-				const driverInfo = driver.Driver;
-				let team = '';
-				if (driver.Constructors.length) {
-					const currentTeam = driver.Constructors.pop();
-					team = currentTeam.name;
-				}
-
-				return {
-					id: driverInfo.driverId,
-					name: `${driverInfo.givenName} ${driverInfo.familyName}`,
-					position: driver.position,
-					points: parseInt(driver.points),
-					team
-				};
-			});
-
-			const driversByPlayer = {};
-			const pointsByPlayer = {};
-			const initialPoints = 0;
-			players.forEach(player => {
-				driversByPlayer[player] = formattedData.filter(
-					driver => drafts[player].includes(driver.id)
-				);
-
-				pointsByPlayer[player] = driversByPlayer[player].reduce(
-					(prev, current) => prev + current.points,
-					initialPoints
-				);
-			});
-
-			setPlayers(players);
-			setDrivers(driversByPlayer);
-			setPoints(pointsByPlayer);
-			setLoading(false);
-		}
-
-		getData();
-	}, []);
+	const { loading, players, drivers, points } = useStandings('2022');
 
 	if (!loading) {
-		return players.map(player => 
-			<Column
-				playerName={player}
-				drivers={drivers[player]}
-				points={points[player]}
-			/>
-		);
+		return (
+			<div className={style.standings}>
+				{players.map(player => 
+					<Column
+						playerName={player}
+						drivers={drivers[player]}
+						points={points[player]}
+					/>
+				)}
+			</div>
+		)
 	} else {
 		return <p>Loading...</p>
 	}
