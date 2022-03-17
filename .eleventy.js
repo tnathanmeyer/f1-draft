d122ca0740dbd4e15820274b1bf26bdaf1afaf33
Nathan Meyer <55331718+tnathanmeyer@users.noreply.github.com>
2022-03-17 16:33:08 -0400

    A bunch more cool setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,34 @@
+const sass = require('sass');
+const htmlmin = require('html-minifier');
+const CleanCSS = require('clean-css');
+
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addTransform("htmlmin", htmlminTransform);
+
+	eleventyConfig.addFilter('cssmin', function (code) {
+		const compiled = sass.compileString(code);
+		return new CleanCSS({}).minify(compiled.css).styles;
+	});
+
+	eleventyConfig.addWatchTarget('./src/scss');
+
 	return {
 		dir: {
-			input: "src"
+			input: 'src',
+			output: 'dist'
 		}
 	}
 };
+
+// 'stolen' from https://github.com/tmns/11ty-feedback/blob/main/.eleventy.js
+function htmlminTransform(content, outputPath) {
+	if (outputPath.endsWith(".html")) {
+		let minified = htmlmin.minify(content, {
+			useShortDoctype: true,
+			removeComments: true,
+			collapseWhitespace: true
+		});
+		return minified;
+	}
+	return content;
+}
