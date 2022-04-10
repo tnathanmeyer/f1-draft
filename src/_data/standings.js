ce71aba458bf59f040052f505224e1aea87d14a2
Nathan Meyer <55331718+tnathanmeyer@users.noreply.github.com>
2022-04-10 16:56:19 -0400

    Got some images loading in + styling + team fix

--- src/_data/standings.js
+++ src/_data/standings.js
@@ -15,16 +15,26 @@ module.exports = async function() {
 		const driverInfo = driver.Driver;
 		
 		let team = 'N/A';
+		console.log(driver.Constructors.length);
 		if (driver.Constructors.length) {
-			const currentTeam = driver.Constructors.pop();
+			const currentTeam = driver.Constructors.slice(-1)[0];
 			team = currentTeam.name;
 		}
 
+		const { givenName, familyName } = driverInfo;
+		const imgName = `${givenName}-${familyName}`
+			.toLowerCase()
+			.normalize("NFD")
+			.replace(/[\u0300-\u036f]/g, "");
+		const imgSrc = `https://www.formula1.com/content/fom-website/en/drivers/${imgName}/_jcr_content/image.img.640.medium.jpg/1646750994602.jpg`
+
+		
 		return {
 			id: driverInfo.driverId,
 			name: `${driverInfo.givenName} ${driverInfo.familyName}`,
 			position: driver.position,
 			points: parseInt(driver.points),
+			imgSrc,
 			team
 		};
 	});
